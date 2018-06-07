2c9599a0d613691682155912485c5bf115fec40e
Greg Swallow <gswallow@ivytech.edu>
2018-06-07 15:38:35 -0400

    Use local file resource.  Duh :)

--- key.tf
+++ key.tf
@@ -2,12 +2,21 @@ resource "tls_private_key" "ansible_git" {
   count = "${var.CREATE_ANSIBLE_GIT_KEY == "true" ? 1 : 0}"
   algorithm = "RSA"
   rsa_bits = 4096
-  provisioner "local-exec" { 
-    command = "echo ${tls_private_key.ansible_git.private_key_pem} > ${var.ANSIBLE_GIT_KEY} && chmod 600 ${var.ANSIBLE_GIT_KEY}"
+}
+
+resource "local_file" "ansible_git" {
+  count = "${var.CREATE_ANSIBLE_GIT_KEY == "true" ? 1 : 0}"
+  content = "${tls_private_key.ansible_git.private_key_pem}"
+  filename = "${format("%s/.ssh/%s", pathexpand("~"), var.ANSIBLE_GIT_KEY)}"
+  provisioner "local-exec" {
+    command = "chmod 600 ${format("%s/.ssh/%s", pathexpand("~"), var.ANSIBLE_GIT_KEY)}"
   }
-  provisioner "local-exec" { 
-    command = "echo ${tls_private_key.ansible_git.public_key_openssh} > ${var.ANSIBLE_GIT_KEY}.pub"
-  }
+}
+
+resource "local_file" "ansible_git_public" {
+  count = "${var.CREATE_ANSIBLE_GIT_KEY == "true" ? 1 : 0}"
+  content = "${tls_private_key.ansible_git.public_key_openssh}"
+  filename = "${format("%s/.ssh/%s.pub", pathexpand("~"), var.ANSIBLE_GIT_KEY)}"
 }
 
 # Seems to me like naming a data source "tls_public_key" when it really extracts private key data is a terraform bug
